f766ace4b230cf27283d1576e50d45785cd8ac58
Will Dennis <williamldennis@gmail.com>
2025-06-02 14:49:17 -0400

    clicking the squares puts x and o

--- src/App.tsx
+++ src/App.tsx
@@ -1,25 +1,54 @@
 import { useState } from 'react'
 import './App.css'
-import { initialGameState, type Cell } from './game'
+import { initialGameState, move, type Cell, type Game, type Row } from './game'
 
+type CellProps = {
+  cell: Cell,
+  cellIndex: number,
+  cellClick?: () => void
+}
+function Cell({ cell, cellIndex, cellClick }: CellProps) {
+  return (
+    <div
+      key={cellIndex}
+      className='p-10 border'
+      onClick={cellClick}
+    >
+      {cell}
+    </div>)
+}
+
+type RowProps = {
+  row: Row
+  rowIndex: number
+  handleMove: (rowIndex: number, colIndex: number) => void
+}
+
+function Row({ row, rowIndex, handleMove }: RowProps) {
+  return (
+    <div key={rowIndex} className='flex justify-center'>
+      {row.map((cell, cellIndex) => <Cell cell={cell} cellIndex={cellIndex} cellClick={() => handleMove(rowIndex, cellIndex)} />)}
+    </div>
+  )
+}
 
 function App() {
-
   const [game, setGame] = useState(initialGameState())
 
-  const cellToCell = (cell: Cell, cellIndex: number) => {
-    return <div className='p-10 border'>{cellIndex} {cell}</div>
-  }
-
-  function rowToRow(row: Cell[], rowIndex: number) {
-    return <div className='flex justify-center align middle'>{row.map(cellToCell)}<br /></div>
+  const handleMove = (rowIndex: number, colIndex: number) => {
+    const nextGame = move(game, rowIndex, colIndex)
+    setGame(nextGame)
   }
 
   return (
     <>
-      <h1 className='font-bold text-4xl'>Jill Jack Joe</h1> 
-      <p className='font-semibold text-sm p-10'>Line up 3 Jacks or 3 Jills. Win a cup of joe.</p>
-      {game.board.map(rowToRow)}
+      <h1 className='font-bold text-4xl'>Jill Jack Joe</h1>
+
+      <p className='text-sm mt-3'>Line up 3 Jacks or 3 Jills. Win a cup of joe.</p>
+      <p className='text-2xl m-6'>
+        Turn: Player {game.currentPlayer}
+      </p>
+      {game.board.map((row, rowIndex) => <Row row={row} rowIndex={rowIndex} handleMove={handleMove} />)}
     </>
   )
 }
--- src/game.ts
+++ src/game.ts
@@ -5,7 +5,6 @@
 
 export type Board = Row[]
 export type Row = Cell[]
-export type CellIndex = [0, 1, 2, 3, 4, 5, 6, 7, 8]
 export type Player = 'x' | 'o'
 export type Cell = Player | null
 export type EndState = Player | 'tie' | undefined
@@ -17,9 +16,19 @@ export type Game = {
 }
 
 export const initialGameState = (): Game => {
-    const game: Game =  {
-        board: [[null, null, null], [null, null, null], [null, null, null]], 
+    const game: Game = {
+        board: [[null, null, null], [null, null, null], [null, null, null]],
         currentPlayer: 'x'
     }
     return game
+}
+
+
+export const move = (game: Game, rowIndex: number, colIndex: number): Game => {
+    const nextGame = structuredClone(game)
+    nextGame.board[rowIndex][colIndex] = game.currentPlayer
+    nextGame.currentPlayer = nextGame.currentPlayer === 'x' ? 'o' : 'x'
+    console.log("click");
+
+    return nextGame
 }
