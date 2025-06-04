1c59639cde71949820c967e6bc9205ac51518d27
Will Dennis <williamldennis@gmail.com>
2025-06-04 14:12:26 -0400

    game seems to be working on server

--- server.ts
+++ server.ts
@@ -5,6 +5,25 @@ import { InMemoryTicTacToeApi } from "./src/api"
 
 const app = express();
 
+app.use(express.json())
+
+const api = new InMemoryTicTacToeApi()
+
 app.get("/message", (_, res) => res.send("Hello express guys!"));
+
+app.get("/api/game/:gameId", async (req, res) => {
+    const game = await api.getGame(req.params.gameId) // the request parameters are the URL gameID
+    res.json(game) // give game with that ID back
+})
+
+app.post("/api/game", async (req, res) => {
+    const game = await api.createGame()
+    res.json(game)
+})
+
+app.post("/api/game/:gameId/move", async (req, res) => {
+    const game = await api.makeMove(req.params.gameId, req.body.row, req.body.col)
+    res.json(game)
+})
 
 ViteExpress.listen(app, 3000, () => console.log("Server is listening..."));
