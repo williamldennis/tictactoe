4a116e7c3b1ec2568edbfb36534e6c55da61a671
Will Dennis <williamldennis@gmail.com>
2025-06-05 16:32:28 -0400

    styled the lobby list

--- src/GameLobby.tsx
+++ src/GameLobby.tsx
@@ -12,13 +12,25 @@ export default function GameLobby() {
 
     return (
         <>
-            <div className="text-black">Game Lobby</div>
-            <p>List of Games</p>
-            {games.map(game => (
-                <div key={game.id}>
-                    <Link to={`/game/${game.id}`}>{game.id}</Link>
+            <div className="rounded-4xl bg-blue-200">
+                <h1 className="text-blue-900 font-bold text-2xl pt-8 pb-5">Available Games</h1>
+                <h1 className="text-blue-900 text-sm">Click a GameID to play</h1>
+                <div className="flex flex-col items-center">
+                    {games.map(game => (
+                        <Link to={`/game/${game.id}`}>
+                            <div
+                                className=" hover:bg-sky-900 border font-medium border-blue-800 w-100 p-4 m-3 rounded bg-blue-800 flex flex-col items-start"
+                                key={game.id}>
+                                <p className="text-xs text-white font-bol">GameID:</p>
+                                <p className="text-md text-blue-400 font-mono">{game.id}</p>
+                            </div>
+                        </Link>
+                    ))}
+
                 </div>
-            ))}
+            </div>
+
+
         </>
 
     )
